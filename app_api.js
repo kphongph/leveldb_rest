96805b23562cc9b449628f07804de912a42c169f
Theerawut <theerawutt53@gmail.com>
2016-12-29 05:25:56 +0700

    fix authen for upload image api

--- app_api.js
+++ app_api.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var passport = require('passport');
+var LocalStrategy = require('passport-localapikey').Strategy;
+var session = require('express-session');
+var authorization = require('express-authorization');
 var https = require('https');
 var path = require('path');
 var fs = require('fs');
@@ -15,6 +19,11 @@ var HOST = process.env.HOST || '';
 
 var app = express();
 
+app.use(session({
+  secret: config.session_key
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -37,13 +46,24 @@ options = {
 };
 /*---ssl certificate---*/
 
+passport.use(new LocalStrategy(function(apikey, done) {
+  login._isAuthen(apikey, done)
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  login._getUser(id, done);
+});
 
 app.get('/img', function(req, res) {
   res.writeHead(200, {
     'content-type': 'text/html'
   });
   res.end(
-    '<form action="/upload/databasebackup?apikey=dc6604a0caba11e690c84b1a15c779ac" enctype="multipart/form-data" method="post">' +
+    '<form action="/upload/databasebackup?apikey=b3ab8110cbfb11e6b935495cf12a55bb" enctype="multipart/form-data" method="post">' +
     '<input type="text" name="title"><br>' +
     '<input type="file" name="upload"><br>' +
     '<input type="submit" value="Upload">' +
@@ -51,7 +71,10 @@ app.get('/img', function(req, res) {
   );
 });
 
-app.post('/upload/:container/:filename?', function(req, res) {
+app.post('/upload/:container/:filename?', 
+passport.authenticate('localapikey', {
+  session: true
+}), function(req, res) {
   //var isAuthen = login._isAuthen(apikey, done);	
   var blobService = azure.createBlobService(config.azure_blob_accountName, config.azure_blob_accessKey);
   var container = req.params.container;
