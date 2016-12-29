a25ce88a81cabaa7f3656b62964201b4ea2d251e
Theerawut <kphongph@gmail.com>
2016-12-29 11:35:01 +0700

    recursive copy

--- copy_to_index.js
+++ copy_to_index.js
@@ -1,11 +1,14 @@
 var levelup = require('levelup');
 var subindex = require('leveldb-index');
+var fs = require('fs');
 
 var copy = function(src,tar,cb) {
   var count = 0;
   var end = false;
   var records = 0;
+  var copying = false;
   src.createReadStream().on('data',function(data) {
+    copying = true;
     count++;
     records++;
     tar.put(data.key,data.value,function(err,result) {
@@ -15,8 +18,12 @@ var copy = function(src,tar,cb) {
       }
     });
   }).on('end',function() {
-    console.log('Total:',records);
-    end=true;
+    if(!copying) {
+      cb();
+    } else {
+      console.log('Total:',records);
+      end=true;
+    }
   });
 }
 
@@ -29,17 +36,39 @@ var count = function(db,cb) {
   });
 }
 
-var src_str = process.argv[2];
 
-var src_db = levelup('./databases/'+src_str,{valueEncoding:'json'});
-var _tar = levelup('./databases/'+src_str+'_i',{valueEncoding:'json'});
-var tar_db = subindex(_tar);
+var clone = function(dir_src,dir_dest,file_name,cb) {
+  var src_db = levelup('./'+dir_src+'/'+file_name,{valueEncoding:'json'});
+  var _tar = levelup('./'+dir_dest+'/'+file_name,{valueEncoding:'json'});
+  var tar_db = subindex(_tar);
 
-console.log('Copying From',src_str,'To',src_str+'_i');
+  console.log('Copying From',file_name);
 
-copy(src_db,tar_db,function() {
-  console.log('done!');
-  count(tar_db,function(records) {
-    console.log('',records,'in '+src_str+'_i');
+  copy(src_db,tar_db,function() {
+    console.log('done!');
+    count(tar_db,function(records) {
+      console.log('',records,'in '+file_name);
+      cb();
+    });
+  });
+};
+
+
+var db_dir = 'databases';
+
+var sync = function(dir_src,dir_dest,list,cb) {
+  var file = list.pop(); 
+  if(!file) { 
+    cb();
+  } else {
+    clone(dir_src,dir_dest,file,function() {
+      sync(dir_src,dir_dest,list,cb);
+    });
+  }
+}
+
+fs.readdir('./'+db_dir,function(err,files) {
+  sync(db_dir,db_dir+'_index',files,function() {
+    console.log('---> Done');
   });
 });
