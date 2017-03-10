a199ea6950c1b6f8a2b72992a64bb4d07c01b612
Theerawut Thaweephattharawong <theerawutt53@gmail.com>
2017-03-11 02:45:02 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,45 +1,72 @@
-var crypto = require('crypto');
+var level = require('level');
+var stream = require('stream');
 var util = require('./util');
-const loginTimeOut = 30;
+var encryption = require('./encryption');
+const loginTimeOut = 180;
+
+var findByUsername = function (username, cb) {
+  util.get_dbs('user_db', function (err, db) {
+    var found = false;
+    var _index = db.indexes['user'];
+    if (_index)
+    var stream = _index.createIndexStream({
+      "start": [username, null],
+      "end": [username, undefined],
+      "limit": 1,
+      "include_doc": true
+    });
+    stream.on('data', function (data) {
+      found = true;
+      cb(found, data);
+    });
+    stream.on('end', function (data) {
+      if (!found) cb(found, null);
+    });
+  });
+}
+
+var findByID = function (id, done) {
+  util.get_dbs('user_db', function (err, db_name) {
+    if (err) {
+      res.json({
+        'ok': false,
+        'message': err
+      });
+    } else {
+      db_name.get(id, function (err, value) {
+        if (err) {
+          done(null, false);
+        } else {
+          done(null, value)
+        }
+      });
+    }
+  });
+};
 
 module.exports = {
-  _login: function(req, res) {
-
-    var pass = crypto.createHmac('sha256', 'inf@rva+')
-      .update(req.body.user + req.body.pass + 'inf@rva+')
-      .digest('hex');
-
-    var key = req.body.user + ':' + pass;
-    console.log(key);
-
-    util.get_dbs('user_index_db', function(err, user_index_db) {
-      if (err) {
-        res.json({
-          'ok': false,
-          'message': err
-        });
-      } else {
-        user_index_db.get(key, function(err, value) {
-          if (err) {
-            res.json({
-              status: false,
-              message: 'Invalid Username & password'
-            });
-          } else {
-            key = value;
+  _login: function (req, res) {
+    var _username = req.body.user;
+    var _pass = req.body.pass;
+    findByUsername(_username, function (found, user) {
+      if (found) {
+        var _password_hash = encryption.password_hash(_pass, user.value.doc.Pass_Salt);
+        if (user.value.doc.User === _username && user.value.doc.Pass_Hash === _password_hash) {
+          if( ! user.value.doc.deactive ){
+            var key = user.value.key;
             var obj = {
-              timestamp: new Date(),
+              timestamp: new Date().getTime(),
               key: key
             };
-
-            util.get_dbs('authen_db', function(err, authen_db) {
+            util.get_dbs('authen_db', function (err, authen_db) {
               if (err) {
                 res.json({
                   'ok': false,
                   'message': err
                 });
               } else {
-                authen_db.put(key, obj, function(err) {
+                authen_db.put(key, obj, function (err) {
+                  console.log({'username':_username,message:'Authenthicated','last_login':new Date(obj.timestamp)});
                   if (err) {
                     res.json({
                       status: false
@@ -53,20 +80,38 @@ module.exports = {
                 });
               }
             });
+          }else{
+            console.log({'username':_username,message:'Username Deactived'});
+            res.json({
+              status: false,
+              message: 'Username Deactived'
+            });
           }
+        }else{
+          console.log({'username':_username,message:'Invalid Username or Password'});
+          res.json({
+            status: false,
+            message:'Invalid Username or Password'
+          });
+        }
+      } else {
+        console.log({'username':_username,message:'Username is Not Found'});
+        res.json({
+          status: false,
+          message:'Username is Not Found'
         });
       }
     });
   },
-  _logout: function(req, res) {
-    util.get_dbs('authen_db', function(err, authen_db) {
+  _logout: function (req, res) {
+    util.get_dbs('authen_db', function (err, authen_db) {
       if (err) {
         res.json({
           'ok': false,
           'message': err
         });
       } else {
-        authen_db.del(req.body.key, function(err) {
+        authen_db.del(req.body.key, function (err) {
           if (err) {
             res.json({
               status: false
@@ -80,62 +125,39 @@ module.exports = {
       }
     });
   },
-  _getUser: function(key, done) {
-    util.get_dbs('user_db', function(err, user_db) {
+  _getUser: function (id, done) {
+    findByID(id, function (err, value) {
       if (err) {
-        res.json({
-          'ok': false,
-          'message': err
-        });
+        done(null, false);
       } else {
-        user_db.get(key, function(err, value) {
-          if (err) {
-            done(null, false);
-          } else {
-            value.id = key;
-            value.permissions = ['noun:*'];
-            done(null, value)
-          }
-        });
+        done(null, value)
       }
     });
   },
-  _isAuthen: function(apikey, done) {
-    util.get_dbs('authen_db', function(err, authen_db) {
+  _isAuthen: function (id, done) {
+    util.get_dbs('authen_db', function (err, authen_db) {
       if (err) {
         res.json({
           'ok': false,
           'message': err
         });
       } else {
-        authen_db.get(apikey, function(err, value) {
+        authen_db.get(id, function (err, value) {
           if (err) {
             done(null, false);
           } else {
-            var diff = Math.abs(new Date(value.timestamp) - new Date());
+
+            var diff = Math.abs(new Date(value.timestamp) - new Date().getTime());
             var minutes = Math.floor((diff / 1000) / 60);
 
             if (minutes > loginTimeOut) {
               done(null, false);
             } else {
-              var key = value.key;
-
-              util.get_dbs('user_db', function(err, user_db) {
+              findByID(id, function (err, value) {
                 if (err) {
-                  res.json({
-                    'ok': false,
-                    'message': err
-                  });
+                  done(null, false);
                 } else {
-                  user_db.get(apikey, function(err, value) {
-                    if (err) {
-                      done(null, false);
-                    } else {
-                      value.id = key;
-                      value.permissions = ['noun:*'];
-                      done(null, value);
-                    }
-                  });
+                  done(null, value)
                 }
               });
             }
