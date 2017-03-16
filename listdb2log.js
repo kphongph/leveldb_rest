4394ea3561b6798abeff845d711f107a90093c56
theerawut Thaweephattharawong <theerawutt53@gmail.com>
2017-03-17 00:09:38 +0700

    listdb2log

--- listdb2log.js
+++ listdb2log.js
@@ -1,34 +1,10 @@
 var list_db_log = [
-  "admin_area",
-  "admin_school",
-  "announcement",
-  "attendance",
-  "authen_db",
-  "desirecharacteristicdata",
-  "earlywarning",
-  "edu_students",
-  "examratio",
-  "form_template",
-  "healthcare",
-  "hostconfig",
-  "hostsummary",
-  "hostsystem",
-  "knowledgestructure",
-  "morning",
-  "morningdetail",
-  "newindicator",
-  "obec_grade",
-  "obec_teacher",
-  "readthinkwritedata",
-  "school",
-  "studentattend",
-  "teacher67p",
-  "user_db",
-  "weightheight"];
+  "form_record",
+  "obec_students"];
 
 module.exports = {
   isdb_log : function(db_name) {
-    if(list_db_log.indexOf(db_name) == -1) return false
+    if(list_db_log.indexOf(db_name) != -1) return false
     else return true
   }
 };
