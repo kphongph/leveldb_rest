5c161b806ddbe77e95bbd6a83ad0ff6db0bc1f94
theerawutt53 <theerawutt53@gmail.com>
2019-03-25 08:11:22 +0000

    addd

---
+++ ssl_option.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var fs = require('fs');
+
+var certsPath = path.join(__dirname, 'ssl_certificate', 'server');
+var caCertsPath = path.join(__dirname, 'ssl_certificate', 'ca');
+var certsJWT = path.join(__dirname, 'ssl_certificate', 'jwt');
+
+module.exports.options = {
+  /*---ssl certificate---*/
+  key: fs.readFileSync(path.join(certsPath, 'server.key')),
+  cert: [
+    fs.readFileSync(path.join(certsPath, 'server-letencrypt.crt')),
+    //fs.readFileSync(path.join(certsPath, 'server-maas.crt'))
+  ],
+  ca: [
+    fs.readFileSync(path.join(caCertsPath, 'ca-letencrypt.crt')),
+    //fs.readFileSync(path.join(caCertsPath, 'ca-maas.crt'))
+  ],
+  requestCert: false,
+  rejectUnauthorized: true
+  /*---ssl certificate---*/
+};
+
+module.exports.jwt = {
+  key: fs.readFileSync(path.join(certsJWT, 'jwt_key.key')),
+  cert: fs.readFileSync(path.join(certsJWT, 'jwt_cert.crt'))
+};
